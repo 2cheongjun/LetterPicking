57e553dd2466c18fe315c9539c9d64c5288a3f40
2cheongjun <lcj8873@gmail.com>
2022-05-11 15:26:20 +0900

    글자피드 - KingFisher 이미지 적용완료

--- TodoList/MainFeed/firstTabVC.swift
+++ TodoList/MainFeed/firstTabVC.swift
@@ -34,8 +34,10 @@ class firstTabVC: UIViewController{
     // 최초에 화면을 실행할때 이미 1페이지에 해당하는 데이터를 읽어 왔으므로,page의 초기값으로 1을 할당하는것이 맞다.
     var page = 1
     var BASEURL = "http://3.39.79.206/"
-    //이미지캐시
-    lazy var cache: NSCache<AnyObject, UIImage> = NSCache()
+    
+    //이미지캐시(URL string을 키값으로 구분하고 image를 넣어준다.)
+    private let imageCache = NSCache<NSString, UIImage>()
+    
     // 로그인한 아이디명표기
     @IBOutlet weak var userName: UILabel!
     @IBOutlet var hi: UILabel! // 님 안녕하세요.
@@ -294,8 +296,9 @@ class firstTabVC: UIViewController{
         
     }// 호출메소드끝
     
-    // 이미지 URL로드하기 ***********************************************************************************************8
+    // 이미지 URL로드하기 **********************************************************************************************
     func loadImage(urlString: String, completion: @escaping (UIImage?)-> Void){
+        // url로 가져와 UIImage로 리턴한다.
         
         let sessionConfig = URLSessionConfiguration.default
         let session = URLSession(configuration: sessionConfig)
@@ -306,12 +309,15 @@ class firstTabVC: UIViewController{
             request.httpMethod = "GET"
             
             session.dataTask(with: request) { data, response, error in
-                //                   print( (response as! HTTPURLResponse).statusCode)
+                // print( (response as! HTTPURLResponse).statusCode)
                 // 데이터가 있으면 UIImage로 리턴
                 if let hasData = data {
-                    
                     completion(UIImage(data: hasData))
+                    
+                    //                    print("hasData: \(hasData)")
                     return
+                    
+                    
                 }
             }.resume() //실행한다.
             session.finishTasksAndInvalidate()
@@ -459,136 +465,159 @@ extension firstTabVC: UITableViewDelegate, UITableViewDataSource{
         cell.priceLabel.text =  self.feedModel?.results[indexPath.row].myPlaceText
         cell.num.text =  self.feedModel?.results[indexPath.row].feedIdx?.description ?? ""
         
-        if (cache.object(forKey: (indexPath as NSIndexPath).row as AnyObject) != nil) {
-            /// 해당 row에 해당되는 부분이 캐시에 존재하는 경우
-            //  cell.imageView?.image = cache.object(forKey: (indexPath as NSIndexPath).row as AnyObject)
-            cell.imageViewLabel.image = cache.object(forKey: (indexPath as NSIndexPath).row as AnyObject)
-        } else {
+        
+        // 킹피셔를 사용한 이미지 처리방법
+        if let imageURL =  self.feedModel?.results[indexPath.row].postImgs  {
             // 이미지처리방법
-            if let hasURL = self.feedModel?.results[indexPath.row].postImgs{
-                // 이미지로드 서버요청
-                self.loadImage(urlString: hasURL) { image in
-                    DispatchQueue.main.async {
-                        
-                        cell.imageViewLabel.image = image
-            
-                    }
+            guard let url = URL(string: imageURL) else {
+                //리턴할 셀지정하기
+                return cell
+            }
+            // 이미지를 다운받는동안 인디케이터보여주기
+            cell.imageViewLabel.kf.indicatorType = .activity
+            print("이미지url \(url)")
+            cell.imageViewLabel.kf.setImage(
+                with: url,
+                placeholder: UIImage(named: "placeholderImage"),
+                  options: [
+                      .scaleFactor(UIScreen.main.scale),
+                      .transition(.fade(1)),
+                      .cacheOriginalImage
+                  ])
+            {
+                result in
+                switch result {
+                    case .success(let value):
+                        print("Task done")
+                    case .failure(let err):
+                        print(err.localizedDescription)
                 }
             }
-            
         }
         
+        // loadImage함수를 이용한 이미지처리방법
+//        if let hasURL = self.feedModel?.results[indexPath.row].postImgs{
+//            print("이미지url \(hasURL)")
+//
+//            //이미지로드 서버요청
+//            self.loadImage(urlString: hasURL) { image in
+//                DispatchQueue.main.async {
+//                    cell.imageViewLabel.image = image
+//                }
+//            }
+//        }
+
+    
+    //        //좋아요*********************************************************************************
+    if likes[indexPath.row] == 1 {
+        cell.isTouched = true
+        numIdx  =  self.feedModel?.results[indexPath.row].feedIdx?.description ?? ""
+        print("게시글 true :\(numIdx)")
         
-        //        //좋아요*********************************************************************************
-        if likes[indexPath.row] == 1 {
-            cell.isTouched = true
-            numIdx  =  self.feedModel?.results[indexPath.row].feedIdx?.description ?? ""
-            print("게시글 true :\(numIdx)")
-            
-        }else{
-            cell.isTouched = false
+    }else{
+        cell.isTouched = false
+    }
+    //
+    return cell
+}
+
+
+//하트 업로드API
+func uploadHeart(postIdx: String?, success: (()->Void)? = nil, fail: ((String)->Void)? = nil) {
+    
+    // userID, postText,이미지묶음을 파라미터에 담아보냄
+    let userID = plist.string(forKey: "name")
+    
+    // 선택한 셀의 게시글 번호를 가져오는 법 생각하기
+    let param: Parameters = [  "cbHeart" : true,
+                               "postIdx" : numIdx ?? 0 ,
+                               "userID" : userID ?? ""]
+    
+    print(" API좋아요:\(param)")
+    // API 호출 URL
+    let url = self.BASEURL+"post/0iOS_feedLike.php"
+    
+    //이미지 전송
+    let call = AF.request(url, method: .post, parameters: param,
+                          encoding: JSONEncoding.default)
+    //                call.responseJSON { res in
+    call.responseJSON { [self] res in
+        
+        // 성공실패케이스문 작성하기
+        //            print("서버로 보냄!!!!!")
+        //            print("JSON= \(try! res.result.get())!)")
+        
+        guard (try! res.result.get() as? NSDictionary) != nil else {
+            print("올바른 응답값이 아닙니다.")
+            return
         }
-        //
-        return cell
+        
+        if let jsonObject = try! res.result.get() as? [String :Any]{
+            let success = jsonObject["success"] as? Int ?? 0
+            
+            if success == 1 {
+                self.alert("좋아요업로드 성공 JSON= \(try! res.result.get())!)")
+                self.dismiss(animated: true, completion: nil)
+                
+                // 이거땜에 좋아요가 두번눌리고 오류냠
+                //                    DispatchQueue.main.async {
+                //                        // 테이블뷰 갱신 (자동으로 갱신안됨)
+                //                        self.tableView.reloadData()
+                //                    }
+                
+            }else{
+                //sucess가 0이면
+                self.alert("좋아요업로드 응답실패")
+            }
+        }
     }
     
+}//함수 끝
+
+
+//    //하트 Delete API
+func DeleteHeart(postIdx: String?, success: (()->Void)? = nil, fail: ((String)->Void)? = nil) {
+    // userID, postText,이미지묶음을 파라미터에 담아보냄
+    let userID = plist.string(forKey: "name")
     
-    //하트 업로드API
-    func uploadHeart(postIdx: String?, success: (()->Void)? = nil, fail: ((String)->Void)? = nil) {
+    // 선택한 셀의 게시글 번호를 가져오는 법 생각하기
+    let param: Parameters = [
+        "cbHeart" : true,
+        "postIdx" : numIdx ?? 0 ,
+        "userID" : userID ?? ""]
+    
+    print("좋아요취소\(param)")
+    
+    //        print(" API 게시글번호 2 :\(postIdx)")
+    // API 호출 URL
+    let url = self.BASEURL+"post/0iOS_feedLike_delete.php"
+    
+    //이미지 전송
+    let call = AF.request(url, method: .post, parameters: param,
+                          encoding: JSONEncoding.default)
+    //                call.responseJSON { res in
+    call.responseJSON { [self] res in
         
-        // userID, postText,이미지묶음을 파라미터에 담아보냄
-        let userID = plist.string(forKey: "name")
-        
-        // 선택한 셀의 게시글 번호를 가져오는 법 생각하기
-        let param: Parameters = [  "cbHeart" : true,
-                                   "postIdx" : numIdx ?? 0 ,
-                                   "userID" : userID ?? ""]
-        
-        print(" API좋아요:\(param)")
-        // API 호출 URL
-        let url = self.BASEURL+"post/0iOS_feedLike.php"
-        
-        //이미지 전송
-        let call = AF.request(url, method: .post, parameters: param,
-                              encoding: JSONEncoding.default)
-        //                call.responseJSON { res in
-        call.responseJSON { [self] res in
-            
-            // 성공실패케이스문 작성하기
-            //            print("서버로 보냄!!!!!")
-            //            print("JSON= \(try! res.result.get())!)")
-            
-            guard (try! res.result.get() as? NSDictionary) != nil else {
-                print("올바른 응답값이 아닙니다.")
-                return
-            }
-            
-            if let jsonObject = try! res.result.get() as? [String :Any]{
-                let success = jsonObject["success"] as? Int ?? 0
-                
-                if success == 1 {
-                    self.alert("좋아요업로드 성공 JSON= \(try! res.result.get())!)")
-                    self.dismiss(animated: true, completion: nil)
-                    
-                    // 이거땜에 좋아요가 두번눌리고 오류냠
-                    //                    DispatchQueue.main.async {
-                    //                        // 테이블뷰 갱신 (자동으로 갱신안됨)
-                    //                        self.tableView.reloadData()
-                    //                    }
-                    
-                }else{
-                    //sucess가 0이면
-                    self.alert("좋아요업로드 응답실패")
-                }
-            }
+        guard (try! res.result.get() as? NSDictionary) != nil else {
+            print("올바른 응답값이 아닙니다.")
+            return
         }
         
-    }//함수 끝
-    
-    
-    //    //하트 Delete API
-    func DeleteHeart(postIdx: String?, success: (()->Void)? = nil, fail: ((String)->Void)? = nil) {
-        // userID, postText,이미지묶음을 파라미터에 담아보냄
-        let userID = plist.string(forKey: "name")
-        
-        // 선택한 셀의 게시글 번호를 가져오는 법 생각하기
-        let param: Parameters = [
-            "cbHeart" : true,
-            "postIdx" : numIdx ?? 0 ,
-            "userID" : userID ?? ""]
-        
-        print("좋아요취소\(param)")
-        
-        //        print(" API 게시글번호 2 :\(postIdx)")
-        // API 호출 URL
-        let url = self.BASEURL+"post/0iOS_feedLike_delete.php"
-        
-        //이미지 전송
-        let call = AF.request(url, method: .post, parameters: param,
-                              encoding: JSONEncoding.default)
-        //                call.responseJSON { res in
-        call.responseJSON { [self] res in
+        if let jsonObject = try! res.result.get() as? [String :Any]{
+            let success = jsonObject["success"] as? Int ?? 0
             
-            guard (try! res.result.get() as? NSDictionary) != nil else {
-                print("올바른 응답값이 아닙니다.")
-                return
-            }
-            
-            if let jsonObject = try! res.result.get() as? [String :Any]{
-                let success = jsonObject["success"] as? Int ?? 0
-                
-                if success == 1 {
-                    self.alert("좋아요취소성공 JSON= \(try! res.result.get())!)")
-                    self.dismiss(animated: true, completion: nil)
-                    //                    }
-                }else{
-                    //sucess가 0이면
-                    self.alert("0")
-                }
+            if success == 1 {
+                self.alert("좋아요취소성공 JSON= \(try! res.result.get())!)")
+                self.dismiss(animated: true, completion: nil)
+                //                    }
+            }else{
+                //sucess가 0이면
+                self.alert("0")
             }
         }
-        
-    }//함수 끝
+    }
+    
+}//함수 끝
 }
 
 // 이미지캐시작업을 위한 싱글톤 클래스생성
